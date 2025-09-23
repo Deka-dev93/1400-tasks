0a499ecbc22fd20940f0190f17d08c473bd9282a
deka <kirillyanovv@gmail.com>
2025-09-23 14:28:22 +0300

    Update file16.py

--- file16.py
+++ file16.py
@@ -3,5 +3,5 @@ v = int(input("v = "))
 x = int(input("x = "))
 y = int(input("y = "))
 
-print("a) 5 10","b) 100", t,"c)", x, "25")
+print("a) 5 10","б) 100", t,"в)", x, "25")
 print(" "," 7 см"," "," 1949", v, "   " + str(x), y)
