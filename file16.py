c820cf093868571294a170878ecbe85daa6bb7fe
deka <kirillyanovv@gmail.com>
2025-09-23 14:20:37 +0300

    Add files via upload

---
+++ file16.py
@@ -0,0 +1,7 @@
+t = int(input("t = "))
+v = int(input("v = "))
+x = int(input("x = "))
+y = int(input("y = "))
+
+print("a) 5 10","b) 100", t,"c)", x, "25")
+print(" "," 7 см"," "," 1949", v, "   " + str(x), y)
