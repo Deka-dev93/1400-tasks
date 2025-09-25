c7356d14caab85edd91263209e6ac32596f28e40
deka <kirillyanovv@gmail.com>
2025-09-25 12:35:24 +0300

    Add files via upload

---
+++ file2.3.py
@@ -0,0 +1,7 @@
+from math import sqrt, sin
+a= float(input("Введите a: "))
+y1 = abs(2*a + sin(abs(3*a)))/ 3,56
+print("Результат a) ",y1 )
+x= float(input("Введите x: "))
+y2 = (3.2 + sqrt(1+x)) / (sin(5*x)/ x)
+print("Результат б) ", y2)
