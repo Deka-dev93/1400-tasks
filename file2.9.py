f2c4ab95482900987eb793658e39748a33ba2600
deka <kirillyanovv@gmail.com>
2025-10-02 11:56:53 +0300

    Add files via upload

---
+++ file2.9.py
@@ -0,0 +1,8 @@
+x = float(input("Введите x: "))
+y = float(input("Введите y: "))
+z = 2*x**3 - 3.44*x*y + 2.3*x**2 - 7.1*y + 2
+print("z =", z)
+a = float(input("Введите a: "))
+b = float(input("Введите b: "))
+x = 3.14*(a + b)**3 + 2.75*b**2 - 12.7*a - 4.1
+print("x =", x)
