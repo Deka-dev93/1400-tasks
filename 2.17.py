742832bde0c41fc558ea2037920d45be8e189783
deka <kirillyanovv@gmail.com>
2025-10-11 19:07:23 +0300

    Add files via upload

--- 2.17.py
+++ 2.17.py
@@ -1,7 +1,16 @@
-from math import sqrt
-a = float(input("Большее основание: "))
-b = float(input("Меньшее основание: "))
-h = float(input("Высота: "))
-c = sqrt(((a - b) / 2)**2 + h**2)
-P = a + b + 2 * c
-print("Периметр :", P)
+import math
+x = float(input("Введите x: "))
+y = float(input("Введите y: "))
+q = 7.25 * math.sin(x) - abs(y)
+print("q =", q)
+if x == 0:
+    print("z: нельзя делить на ноль")
+else:
+    znam = y + 1 / math.sqrt(x * x + 10)
+    if znam <= 0:
+        print("z: корень из отрицательного числа")
+    else:
+        chisl = x + (2 + y) / (x * x)
+        znam = math.sqrt(znam)
+        z = chisl / znam
+        print("z =", z)
