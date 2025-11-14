bb9ad0b17ac2505e992cee3adc39df3c8fb6eb3f
deka <kirillyanovv@gmail.com>
2025-11-14 13:57:38 +0300

    Add files via upload

---
+++ 2.20.py
@@ -0,0 +1,11 @@
+import math
+e = float(input("Введите значение e: "))
+f = float(input("Введите значение f: "))
+g = float(input("Введите значение g: "))
+h = float(input("Введите значение h: "))
+a = math.sqrt(e - (3/f)**3 + g)
+b = math.sin(e) + math.cos(h)**2
+c = (33 * g) / (e * f - 3)
+print("a = ",a)
+print("b = ",b)
+print("c = ",c)
